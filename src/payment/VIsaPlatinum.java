a4ce8d30f948d7ee96980d527407e89c03991fce
Asylbek Choturaliev <asikosan2014@gmail.com>
2024-01-24 17:15:03 +0600

    scrum-162

---
+++ src/payment/VIsaPlatinum.java
@@ -0,0 +1,8 @@
+package payment;
+
+public class VIsaPlatinum {
+    public void paymentVisaPlatinum(String CardholderPl, int cardNumberPl){
+        System.out.println(CardholderPl + " " + cardNumberPl);
+    }
+
+}
