1acb16f8d1cbe0e0c0defd69dfb5d354fbe7c610
Ruskano <ruskano1991@gmail.com>
2024-01-27 17:07:43 +0600

    C1 - commit -C3

--- src/c1/C1.java
+++ src/c1/C1.java
@@ -4,4 +4,8 @@ public class C1 {
     public static void main(String[] args) {
         System.out.println("Hi");
     }
+
+    public static void SayHello (){
+        System.out.println("Say Hello");
+    }
 }
