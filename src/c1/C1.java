70a320cf1aa805d91fc53d3c7576763a7187a34d
Ruskano <ruskano1991@gmail.com>
2024-01-27 17:09:28 +0600

    Revert "C1 - commit updated"
    This reverts commit 1acb16f8d1cbe0e0c0defd69dfb5d354fbe7c610.

--- src/c1/C1.java
+++ src/c1/C1.java
@@ -4,8 +4,4 @@ public class C1 {
     public static void main(String[] args) {
         System.out.println("Hi");
     }
-
-    public static void SayHello (){
-        System.out.println("Say Hello");
-    }
 }
